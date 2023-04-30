47e054ecaa9fab464e3818507d70d40fdbf1f48a
varun <varunvekaria@gmail.com>
2023-04-30 18:09:43 +0530

    "successful search implementation in console"

--- src/pages/imageGallery.js
+++ src/pages/imageGallery.js
@@ -3,15 +3,15 @@ import { useState, useEffect } from "react";
 function ImageGallery() {
 	const [images, setImages] = useState([]);
 	const [count, setcount] = useState("");
-	const [searchWord, setSearchWord] = useState("");
-	const [searchResults, setSearchResults] = useState([]);
+	const [searchText, setSearchText] = useState("");
+	const [common, setCommon] = useState([])
 	// useEffect(() => {
 
 	// 	fetchData();
 	// }, []);
 
 	async function loadImage() {
-		// console.log(count);
+		console.log(count);
 		if (count < 1) {
 			alert("enter value greater or equal to 1");
 		} else {
@@ -19,18 +19,21 @@ function ImageGallery() {
 				`https://www.reddit.com/r/images/top.json?limit=${count}&t=month`
 			);
 			const data = await response.json();
-			// console.log(data.data)
-			// setSearchResults(data.filter(item => item.toLowerCase().includes(searchWord.toLowerCase())));
-			const hello = data.data.children;
 			setImages(data.data.children);
-			console.log(hello);
-			setSearchResults(
-				hello.filter((item) =>
-					item.data.title.toLowerCase().includes(searchWord.toLowerCase())
-				)
-			);
+			setCommon(data.data.children)
+			console.log(images)
 		}
 	}
+
+	function handleSearch(event) {
+		setSearchText(event.target.value);
+		filteredImages
+	}
+	const filteredImages = images.filter((image) =>
+		image.data.title.toLowerCase().includes(searchText.toLowerCase())
+		
+	);
+
 
 	const rows = [];
 
@@ -38,16 +41,6 @@ function ImageGallery() {
 		const row = images.slice(i, i + 3);
 		rows.push(row);
 	}
-
-	const handleSearch = (event) => {
-		setSearchWord(event.target.value);
-		setSearchResults(
-			hello.data.filter((item) =>
-				item.data.title.toLowerCase().includes(event.target.value.toLowerCase())
-			)
-		);
-		console.log(searchResults);
-	};
 
 	return (
 		<div className={`container mx-auto px-4 max-w-6xl m-auto font-bold`}>
@@ -64,10 +57,7 @@ function ImageGallery() {
 				<button type="submit" onClick={loadImage}>
 					Submit
 				</button>
-				<label>
-					Search:
-					<input type="text" value={searchWord} onChange={handleSearch} />
-				</label>
+				<input type="text" value={searchText} onChange={handleSearch} />
 			</div>
 			{rows.map((row, rowIndex) => (
 				<div key={rowIndex} className="flex  justify-center ">
@@ -75,18 +65,18 @@ function ImageGallery() {
 						const checkImg = new Image();
 						// console.log(image.data);
 						checkImg.src = image.data.url;
-						let title = image.data.title;
-
+						let title1 = image.data.title;
+						{console.log(filteredImages)}
 						if (checkImg.complete && checkImg.naturalWidth !== 0) {
 							return (
-								<div>
+								<>
 									<img
 										key={imageIndex}
 										className="w-1/3 shadow-xl	p-30 m-20 rounded-2xl mx-2 my-4  transform hover:scale-90 transition-all duration-500 border-slate-600	border-solid border-2"
 										src={image.data.url}
 									/>
-									<p>{title}</p>
-								</div>
+									<p>{title1}</p>
+								</>
 							);
 						} else {
 							return null;
